c53bccfc5c6b8d4ac01c6eb1dcabad3bd82540f9
mary <mary@mary>
2024-02-12 15:08:29 +0300

    перенесла кастом в графики

--- src/chartEstimate/qcustomplot.cpp
+++ src/chartEstimate/qcustomplot.cpp
@@ -23,7 +23,7 @@
 **          Version: 2.1.1                                                **
 ****************************************************************************/
 
-#include "inc/qcustomplot.h"
+#include "inc/chartEstimate/qcustomplot.h"
 
 
 /* including file 'src/vector2d.cpp'       */
